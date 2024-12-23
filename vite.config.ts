2c4a8370a04eb3db47a02ffb01860576cdc7605a
Magnetic_Soul <everything_90s@yahoo.com>
2024-12-23 16:37:27 -0700

    change local host

--- vite.config.ts
+++ vite.config.ts
@@ -15,12 +15,12 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
-    // proxy: {
-    //   '/api': {
-    //     target: 'http://localhost:3001.',
-    //     changeOrigin: true,
-    //     secure: false,
-    //   },
-    // },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4173.',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
