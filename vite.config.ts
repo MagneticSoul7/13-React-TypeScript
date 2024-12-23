dcf93d7d5b21873c42999db993e8913bd7c951b4
Magnetic_Soul <everything_90s@yahoo.com>
2024-12-23 16:02:45 -0700

    add port

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,25 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   envDir: './env',
+//   plugins: [react()],
+// });
+
+
+
 export default defineConfig({
   envDir: './env',
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
